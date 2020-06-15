6f8831274d82ab85e402ecfb2b7e1830c5cbf1d2
Masha Grom <grom.m.yu@gmail.com>
2020-06-15 16:47:21 +0300

    2222

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,5 +1,4 @@
 import React from "react";
-import PropTypes from "prop-types";
 import styles from "./Contact.module.css";
 
 function Contact({ id, name, number, onRemove }) {
@@ -14,12 +13,5 @@ function Contact({ id, name, number, onRemove }) {
     </li>
   );
 }
-
-Contact.propTypes = {
-  id: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
-  number: PropTypes.string.isRequired,
-  onRemove: PropTypes.func.isRequired,
-};
 
 export default Contact;
