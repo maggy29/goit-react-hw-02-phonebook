1f87e1169c8c7b826464d2c02c8423694f7d22e8
Masha Grom <grom.m.yu@gmail.com>
2020-06-15 16:13:14 +0300

    Updates

---
+++ precache-manifest.924cd12f3c45fb0898030015d51fd5c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91aef154918fb0dcb58fe25b66c2c7b7",
+    "url": "/maggy29.github.io/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "571af3ba0d20fdeb6a9c",
+    "url": "/maggy29.github.io/goit-react-hw-02-phonebook/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "769275e731dddbedfce9",
+    "url": "/maggy29.github.io/goit-react-hw-02-phonebook/static/css/main.e32814a2.chunk.css"
+  },
+  {
+    "revision": "571af3ba0d20fdeb6a9c",
+    "url": "/maggy29.github.io/goit-react-hw-02-phonebook/static/js/2.f4f6f78f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/maggy29.github.io/goit-react-hw-02-phonebook/static/js/2.f4f6f78f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "769275e731dddbedfce9",
+    "url": "/maggy29.github.io/goit-react-hw-02-phonebook/static/js/main.f11fbd1d.chunk.js"
+  },
+  {
+    "revision": "c6e57d751cb9bcd15498",
+    "url": "/maggy29.github.io/goit-react-hw-02-phonebook/static/js/runtime-main.b183d225.js"
+  }
+]);
